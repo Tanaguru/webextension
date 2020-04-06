d36b9820870dc4bfea6897b07b39ab8902a137f2
rgervois-oceane <rgervois@oceaneconsulting.com>
2020-04-06 14:42:27 +0200

    Fonction unique de test de l'exposition de l'élément aux TAs.

--- ressources/scripts/content_scripts/content.js
+++ ressources/scripts/content_scripts/content.js
@@ -710,65 +710,36 @@ if (!HTMLElement.prototype.hasOwnProperty('isNotVisibleDueTo')) {
 	} });
 }
 
-if (!SVGElement.prototype.hasOwnProperty('isNotExposedDueTo')) {
-	Object.defineProperty(SVGElement.prototype, 'isNotExposedDueTo', { get: function () {
-		var result = [];
-		if (this.getAttribute('aria-hidden') == 'true') {
-			result.push('aria:hidden');
-		}
-		else {
-			var ariahiddenfound = false;
-			var pt = this.parentNode;
-			while (pt && pt.nodeType == 1) {
-				if (pt.getAttribute('aria-hidden') == 'true') {
-					ariahiddenfound = true;
-					break;
-				}
-				pt = pt.parentNode;
+var isNotExposedDueTo = function () {
+	var result = [];
+	if (this.getAttribute('aria-hidden') == 'true') {
+		result.push('aria:hidden');
+	}
+	else {
+		var ariahiddenfound = false;
+		var pt = this.parentNode;
+		while (pt && pt.nodeType == 1) {
+			if (pt.getAttribute('aria-hidden') == 'true') {
+				ariahiddenfound = true;
+				break;
 			}
-			if (ariahiddenfound) {
-				result.push('parent-aria:hidden');
-			}
+			pt = pt.parentNode;
 		}
-		if (window.getComputedStyle(this, null).getPropertyValue('display') == 'none') {
-			result.push('css:display');
+		if (ariahiddenfound) {
+			result.push('parent-aria:hidden');
 		}
-		if (window.getComputedStyle(this, null).getPropertyValue('visibility') == 'hidden') {
-			result.push('css:visibility');
-		}
-		return result;
-	} });
-}
+	}
+	if (window.getComputedStyle(this, null).getPropertyValue('display') == 'none') {
+		result.push('css:display');
+	}
+	if (window.getComputedStyle(this, null).getPropertyValue('visibility') == 'hidden') {
+		result.push('css:visibility');
+	}
+	return result;
+};
 
-if (!HTMLElement.prototype.hasOwnProperty('isNotExposedDueTo')) {
-	Object.defineProperty(HTMLElement.prototype, 'isNotExposedDueTo', { get: function () {
-		var result = [];
-		if (this.getAttribute('aria-hidden') == 'true') {
-			result.push('aria:hidden');
-		}
-		else {
-			var ariahiddenfound = false;
-			var pt = this.parentNode;
-			while (pt && pt.nodeType == 1) {
-				if (pt.getAttribute('aria-hidden') == 'true') {
-					ariahiddenfound = true;
-					break;
-				}
-				pt = pt.parentNode;
-			}
-			if (ariahiddenfound) {
-				result.push('parent-aria:hidden');
-			}
-		}
-		if (window.getComputedStyle(this, null).getPropertyValue('display') == 'none') {
-			result.push('css:display');
-		}
-		if (window.getComputedStyle(this, null).getPropertyValue('visibility') == 'hidden') {
-			result.push('css:visibility');
-		}
-		return result;
-	} });
-}
+if (!SVGElement.prototype.hasOwnProperty('isNotExposedDueTo')) Object.defineProperty(SVGElement.prototype, 'isNotExposedDueTo', { get: isNotExposedDueTo });
+if (!HTMLElement.prototype.hasOwnProperty('isNotExposedDueTo')) Object.defineProperty(HTMLElement.prototype, 'isNotExposedDueTo', { get: isNotExposedDueTo });
 
 if (!SVGElement.prototype.hasOwnProperty('canBeReachedUsingKeyboardWith')) {
 	Object.defineProperty(SVGElement.prototype, 'canBeReachedUsingKeyboardWith', { get: function () {
