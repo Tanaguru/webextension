11c8e336681b5ac9f3de927adc0e39010f3bf946
severineFF-tanaguru <sfoucherferet@tanaguru.com>
2021-08-19 18:37:14 +0200

    12.1 to 12.7

--- src/references/rgaa4.js
+++ src/references/rgaa4.js
@@ -6480,7 +6480,141 @@ tanaguruTestsList.push({
 
 /**
  *? NAVIGATION
+ *TODO voir pour tester les liens d'évitement implémenté de façon "classique"
  */
+
+//* 12.1 Chaque ensemble de pages dispose-t-il de deux systèmes de navigation différents, au moins (hors cas particuliers) ?
+// 12.1.1 Chaque ensemble de pages vérifie-t-il une de ces conditions (hors cas particuliers) ?
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Chaque ensemble de pages doit disposer d'au moins deux systèmes de navigation différents.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.1.1'] }
+});
+
+//* 12.2 Dans chaque ensemble de pages, le menu et les barres de navigation sont-ils toujours à la même place (hors cas particuliers) ?
+// 12.2.1 Dans chaque ensemble de pages, chaque page disposant d'un menu ou de barres de navigation vérifie-t-elle ces conditions (hors cas particuliers) ?
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Dans chaque ensemble de pages le menu et les barres de navigation sont toujours à la même place dans la présentation ET se présentent toujours dans le même ordre relatif dans le code source.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.2.1'] }
+});
+
+//* 12.3 La page « plan du site » est-elle pertinente ?
+// 12.3.1 La page « plan du site » est-elle représentative de l'architecture générale du site ?
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "La page « plan du site » doit être représentative de l'architecture générale du site.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.3.1'] }
+});
+
+// 12.3.2 Les liens du plan du site sont-ils fonctionnels ?
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Les liens du plan du site doivent être fonctionnels.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.3.2'] }
+});
+
+// 12.3.3 Les liens du plan du site renvoient-ils bien vers les pages indiquées par l'intitulé ?
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Les liens du plan du site doivent pointer vers les pages indiquées par l'intitulé.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.3.3'] }
+});
+
+//* 12.4 Dans chaque ensemble de pages, la page « plan du site » est-elle atteignable de manière identique ?
+// 12.4.1 Dans chaque ensemble de pages, la page « plan du site » est-elle accessible à partir d'une fonctionnalité identique ?
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Dans chaque ensemble de pages, la page « plan du site » doit être accessible à partir d'une fonctionnalité identique.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.4.1'] }
+});
+
+// 12.4.2 Dans chaque ensemble de pages, la fonctionnalité vers la page « plan du site » est-elle située à la même place dans la présentation ?
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Dans chaque ensemble de pages, la fonctionnalité vers la page « plan du site » doit être située à la même place dans la présentation.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.4.2'] }
+});
+
+// 12.4.3 Dans chaque ensemble de pages, la fonctionnalité vers la page « plan du site » se présente-t-elle toujours dans le même ordre relatif dans le code source ?
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Dans chaque ensemble de pages, la fonctionnalité vers la page « plan du site » doit toujours se présenter dans le même ordre relatif dans le code source.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.4.3'] }
+});
+
+//* 12.5 Dans chaque ensemble de pages, le moteur de recherche est-il atteignable de manière identique ?
+// 12.5.1 Dans chaque ensemble de pages, le moteur de recherche est-il accessible à partir d'une fonctionnalité identique ?
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Dans chaque ensemble de pages, le moteur de recherche doit être accessible à partir d'une fonctionnalité identique.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.5.1'] }
+});
+
+// 12.5.2 Dans chaque ensemble de pages, la fonctionnalité vers le moteur de recherche est-elle située à la même place dans la présentation ?
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Dans chaque ensemble de pages, la fonctionnalité vers le moteur de recherche doit être située à la même place dans la présentation.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.5.2'] }
+});
+
+// 12.5.3 Dans chaque ensemble de pages, la fonctionnalité vers le moteur de recherche se présente-t-elle toujours dans le même ordre relatif dans le code source ?
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Dans chaque ensemble de pages, la fonctionnalité vers le moteur de recherche doit toujours se présenter dans le même ordre relatif dans le code source.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.5.3'] }
+});
+
+//* 12.6 Les zones de regroupement de contenus présentes dans plusieurs pages web (zones d'en-tête, de navigation principale, de contenu principal, de pied de page et de moteur de recherche) peuvent-elles être atteintes ou évitées ?
+// 12.6.1 Dans chaque page web où elles sont présentes, la zone d'en-tête, de navigation principale, de contenu principal, de pied de page et de moteur de recherche respectent-elles au moins une de ces conditions : 
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Les zones de regroupement de contenus présentes dans plusieurs pages web (zones d'en-tête, de navigation principale, de contenu principal, de pied de page et de moteur de recherche) doivent pouvoir être atteintes ou évitées.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.6.1'] }
+});
+
+//* 12.7 Dans chaque page web, un lien d'évitement ou d'accès rapide à la zone de contenu principal est-il présent (hors cas particuliers) ?
+// 12.7.1 Dans chaque page web, un lien permet-il d'éviter la zone de contenu principal ou d'y accéder (hors cas particuliers) ?
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Dans chaque page web, un lien doit permettre d'éviter la zone de contenu principal ou d'y accéder.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.7.1'] }
+});
+
+// 12.7.2 Dans chaque ensemble de pages, le lien d'évitement ou d'accès rapide à la zone de contenu principal vérifient-il ces conditions (hors cas particuliers) ? 
+tanaguruTestsList.push({
+	lang: 'fr',
+	name: "Dans chaque ensemble de pages, le lien d'évitement ou d'accès rapide à la zone de contenu principal doit être situé à la même place dans la présentation  et se présente toujours dans le même ordre relatif dans le code source. Il doit également être visible ou visible à la prise de focus et fonctionnel.",
+    status: 'untested',
+    tags: ['a11y', 'keyboard'],
+    ressources: { 'rgaa': ['12.7.2'] }
+});
 
 //* 12.8 Dans chaque page web, l'ordre de tabulation est-il cohérent ?
 // 12.8.1 Dans chaque page web, l'ordre de tabulation dans le contenu est-il cohérent ?
