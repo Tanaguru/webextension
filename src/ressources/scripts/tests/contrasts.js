263761c08131a7f545b41c6f42ebbc2f266f814b
severineFF-tanaguru <sfoucherferet@tanaguru.com>
2021-10-06 15:00:45 +0200

    fix: contrast .screen-reader-text not visible

--- src/ressources/scripts/tests/contrasts.js
+++ src/ressources/scripts/tests/contrasts.js
@@ -346,14 +346,14 @@ function getOpacity(element) {
 	}
 
 	var regexClipP = /.{6,7}\(0px|.{6,7}\(.+[, ]0px/g; // circle(0) || ellipse(0)
-	var regexClip = /rect\(0px,0px,0px,0px\)/; // rect(0)
+	var regexClip = /rect\([01]px,[01]px,[01]px,[01]px\)/; // rect(0) // rect(1)
 	
 	/**
 	 ** checks if the element is hidden with :
 	 * clip-path: circle(0) || ellipse(0)
-	 * clip: rect(0,0,0,0) && position: absolute
-	 * width: 0 && overflow: hidden
-	 * height: 0 && overflow: hidden
+	 * clip: (rect(0,0,0,0) || rect(1px,1px,1px,1px)) && position: absolute
+	 * width: < 2 && overflow: hidden
+	 * height: < 2 && overflow: hidden
 	 */
 	while(element && element.tagName != 'HTML') {
 		var opacityZero = window.getComputedStyle(element, null).getPropertyValue('opacity').trim() == 0;
@@ -361,8 +361,8 @@ function getOpacity(element) {
 		if(
 			window.getComputedStyle(element, null).getPropertyValue('display').trim() === 'none'
 			|| opacityZero
-			|| (element.offsetWidth === 0 && window.getComputedStyle(element, null).getPropertyValue('overflow').trim() === 'hidden')
-			|| (element.offsetHeight === 0 && window.getComputedStyle(element, null).getPropertyValue('overflow').trim() === 'hidden')
+			|| (element.offsetWidth <= 1 && window.getComputedStyle(element, null).getPropertyValue('overflow').trim() === 'hidden')
+			|| (element.offsetHeight <= 1 && window.getComputedStyle(element, null).getPropertyValue('overflow').trim() === 'hidden')
 			|| window.getComputedStyle(element, null).getPropertyValue('clip-path').match(regexClipP)
 			|| (window.getComputedStyle(element, null).getPropertyValue('clip').replace(/ /g, '').match(regexClip) && window.getComputedStyle(element, null).getPropertyValue('position').trim() === 'absolute')
 		) {
