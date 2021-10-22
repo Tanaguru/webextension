2defc82b1bd92d197d58ab7295b4791cfae45834
severineFF-tanaguru <sfoucherferet@tanaguru.com>
2021-10-22 19:06:53 +0200

    highlight action & manifest permissions

--- src/devtools/tanaguru-devtools.js
+++ src/devtools/tanaguru-devtools.js
@@ -1,14 +1,14 @@
 function handleShown() {
-	browser.runtime.sendMessage({
-		tabId: browser.devtools.inspectedWindow.tabId,
-		command: 'initPopup'
-	});
+	// browser.runtime.sendMessage({
+	// 	tabId: browser.devtools.inspectedWindow.tabId,
+	// 	command: 'initPopup'
+	// });
 }
 
 function handleHidden() {
-	browser.runtime.sendMessage({
+	chrome.runtime.sendMessage({
 		tabId: browser.devtools.inspectedWindow.tabId,
-		command: 'resetPopup'
+		command: 'resetPanel'
 	});
 }
 
@@ -17,8 +17,9 @@ var manifest = chrome.runtime.getManifest();
 chrome.devtools.panels.create(
 	manifest.short_name,
 	'/ressources/images/icons/tanaguru-32-dark.png',
-	'/devtools/panel/tanaguru-devtools-panel.html'
-);/*.then((newPanel) => {
-	newPanel.onShown.addListener(handleShown);
-	newPanel.onHidden.addListener(handleHidden);
-});*/
+	'/devtools/panel/tanaguru-devtools-panel.html',
+	(newPanel) => {
+		// newPanel.onShown.addListener(handleShown);
+		newPanel.onHidden.addListener(handleHidden);
+	}
+);
--- src/ressources/scripts/highlight.js
+++ src/ressources/scripts/highlight.js
@@ -1,10 +1,55 @@
-var element = document.querySelector(element);
-if (!element.hasAttribute('data-tanaguruhighlight')) {
-	var rect = element.getBoundingClientRect();
-	element.setAttribute('data-tanaguruhighlight', 'true');
-	window.scrollTo(0, rect.top);
+var el = document.querySelector(element);
+if(el) {
+	if(el.offsetHeight < 2 || el.offsetWidth < 2) el = el.parentNode;
+	if(el.hasAttribute('data-tanaguruhighlight')) cleanHighlight();
+	else runHighlight();
+} else {
+	let previousHighlight = document.querySelector('[data-tanaguruhighlight]');
+	if(previousHighlight) previousHighlight.removeAttribute('data-tanaguruhighlight');
+	document.querySelectorAll('[data-tng-nohl]').forEach(nohl => {
+		nohl.removeAttribute('data-tng-nohl');
+	});
 }
-else {
-	element.removeAttribute('data-tanaguruhighlight');
-	window.scrollTo(0, 0);
+
+function runHighlight() {
+	let previousHighlight = document.querySelector('[data-tanaguruhighlight]');
+	if(previousHighlight) previousHighlight.removeAttribute('data-tanaguruhighlight');
+	document.querySelectorAll('[data-tng-nohl]').forEach(nohl => {
+		nohl.removeAttribute('data-tng-nohl');
+	});
+	
+	el.setAttribute('data-tanaguruhighlight', 'true');
+	var rect = el.getBoundingClientRect();
+	window.scrollTo(0, rect.top);
+	if(el == document.body) return;
+	var elements = document.body.children;
+	filterParents(elements);
+}
+
+function filterParents(collectionHTML) {
+	var eureka = false;
+	for(let i = 0; i < collectionHTML.length; i++) {
+		if(['noscript', 'script', 'style'].indexOf(collectionHTML[i].tagName.toLowerCase()) == -1) {
+			if(collectionHTML[i] == el) {
+				eureka = true;
+				continue;
+			}
+			else if(collectionHTML[i].querySelector('[data-tanaguruhighlight]')) {
+				if(!eureka) {
+					var subElements = collectionHTML[i].children;
+					filterParents(subElements);
+				}
+			}
+			else {
+				collectionHTML[i].setAttribute('data-tng-nohl', 'true');
+			}
+		}
+	}
+}
+
+function cleanHighlight() {
+	el.removeAttribute('data-tanaguruhighlight');
+	document.querySelectorAll('[data-tng-nohl]').forEach(nohl => {
+		nohl.removeAttribute('data-tng-nohl');
+	});
 }
